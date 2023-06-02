aa4734c9199b9978a0b78f77d8f8749165a7860d
Mohammed Mustapha Kura <mustykmohammed@gmail.com>
2023-06-02 17:37:58 +0530

    Fixed bug a bug for images not rendering on deployment

--- src/components/testimonail.jsx
+++ src/components/testimonail.jsx
@@ -4,7 +4,7 @@ export default function Testimonial(props){
     return(
         <article className={`testimonial--${props.item.id}   ${(props.item.id=== 1 || props.item.id === 4) && "col-span-2"} `} >
             <div className="testimonial--details">
-                <img src={`./src/assets/image-${props.item.pfp}.jpg`}/>
+                <img src={props.item.pfp}/>
                 <div>
                     <h5>{props.item.name}</h5>
                     <p>Verified Graduate</p>
